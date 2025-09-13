05fc816158a35d6e084807cbfef43a7ec5730bfd
Pratham Wakde <144244135+prathamwakde@users.noreply.github.com>
2025-09-13 16:54:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from snake.game import Game, GameConf,GameMode
+
+greedy = "GredySolver"
+hamilton = "HamiltonSolver"
+
+normal = GameMode.NORMAL
+
+conf =  GameConf()
+conf.solver_name = hamilton
+conf.mode = normal
+print("Solver:%s"%(conf.solver_name))
+print("Mode:%s"%(conf.mode))
+Game(conf).run()
